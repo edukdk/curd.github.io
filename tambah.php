cfcda3db5e4c1559f0ce3a8eb3a06c32b5f28c25
edukdk <123381394+edukdk@users.noreply.github.com>
2023-01-23 21:25:46 +0800

    Add files via upload

---
+++ tambah.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DATA MAHASISWA UNIVERSITAS INDONESIA</title>
+	<link rel="stylesheet" type="text/css" href="desaintambah.css">
+</head>
+<body>
+	<h2>DATA MAHASISWA UNIVERSITAS INDONESIA</h2>
+	<br/>
+	<br/>
+	<table><h3>TAMBAH DATA MAHASISWA</h3></table>
+	<form method="post" action="tambah_aksi.php">
+		<table>
+			<tr>
+				<td>Nama</td>
+				<td><input type="text" name="nama"></td>
+			</tr>
+			<tr>
+				<td>NIM</td>
+				<td><input type="text" name="nim"></td>
+			</tr>
+			<tr>
+				<td>Alamat</td>
+				<td><input type="text" name="alamat"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" value="SIMPAN"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
---
+++ tambah_aksi.php
@@ -0,0 +1,16 @@
+<?php 
+// koneksi database
+include 'koneksi.php';
+
+// menangkap data yang dikirim dari form
+$nama = $_POST['nama'];
+$nim = $_POST['nim'];
+$alamat = $_POST['alamat'];
+
+//menginput data ke database
+mysqli_query($koneksi, "insert into mahasiswa values('', '$nama','$nim','$alamat')");
+
+//menghalikan halaman kembali ke index.php
+header("location:index.php")
+ 
+ ?>
